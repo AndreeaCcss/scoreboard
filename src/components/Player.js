b4e244cd08793caaaf500fd7ed5e91f0af2b49d4
andreeaCcss <andreeaccss@gmail.com>
2019-08-20 17:19:08 +0200

    Delete  button update

--- src/components/Player.js
+++ src/components/Player.js
@@ -25,7 +25,7 @@ export default class Player extends Component {
         <p className="name">{ this.props.name } </p>
         <p className="score"> { this.props.score } </p>
         <button className="plus-button" onClick={this.handleClick}>+</button>
-        <button className="delete-button" onClick={this.handleDeleteClick}>-</button>
+        <button className="delete-button" onClick={this.handleDeleteClick}>x</button>
       </li>
     )
   }
